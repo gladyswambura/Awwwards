0d67a456810f499a6f17ff8840bd8d22cd500a6b
gladys <gladyswahito7@gmail.com>
2023-04-04 12:00:33 +0300

    deployment to railways

--- AWWWARDS/settings.py
+++ AWWWARDS/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key') # Instead of you
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
